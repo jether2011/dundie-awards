a2b4f809923db95939d46bb53872e50d9a14543b
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2025-05-01 11:52:46 -0300

    feat: update Employee controller

--- src/main/java/com/ninjaone/dundie_awards/controller/EmployeeController.java
+++ src/main/java/com/ninjaone/dundie_awards/controller/EmployeeController.java
@@ -1,103 +1,119 @@
 package com.ninjaone.dundie_awards.controller;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import com.ninjaone.dundie_awards.controller.response.EmployeeResponse;
+import com.ninjaone.dundie_awards.model.Employee;
+import com.ninjaone.dundie_awards.services.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 import java.util.Optional;
 
-import com.ninjaone.dundie_awards.AwardsCache;
-import com.ninjaone.dundie_awards.MessageBroker;
-import com.ninjaone.dundie_awards.model.Activity;
-import com.ninjaone.dundie_awards.model.Employee;
-import com.ninjaone.dundie_awards.repository.ActivityRepository;
-import com.ninjaone.dundie_awards.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
-@RequestMapping()
+@RestController
+@RequestMapping("/api/v1")
 public class EmployeeController {
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
+    private static final String CREATED_URI = "/api/v1/employees/%s";
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private final EmployeeService employeeService;
 
-    @Autowired
-    private ActivityRepository activityRepository;
-
-    @Autowired
-    private MessageBroker messageBroker;
-
-    @Autowired
-    private AwardsCache awardsCache;
-
-    // get all employees
-    @GetMapping("/employees")
-    @ResponseBody
-    public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
-    // create employee rest api
+    /**
+     * {@code POST  /employees} : Create a new employee.
+     *
+     * @param employee the employee to create
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new employee
+     */
     @PostMapping("/employees")
-    @ResponseBody
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return employeeRepository.save(employee);
+    public ResponseEntity<EmployeeResponse> createEmployee(@Validated @RequestBody Employee employee) throws URISyntaxException {
+        LOG.info("Starting to create employee: {}", employee);
+
+        if (employee.getId() != 0) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        var response = EmployeeResponse.from(employeeService.createEmployee(employee));
+        LOG.info("Employee created: {}", response);
+
+        return ResponseEntity.created(new URI(CREATED_URI.formatted(response.id()))).body(response);
     }
 
-    // get employee by id rest api
+    /**
+     * {@code GET  /employees} : get all employees.
+     *
+     * @param pageable the pagination information
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of employees in body
+     */
+    @GetMapping("/employees")
+    public ResponseEntity<Page<EmployeeResponse>> getAllEmployees(
+            @PageableDefault(page = 0, size = 30, sort = "id") final Pageable pageable
+    ) {
+        LOG.info("Starting to get all employees with pageable: {}", pageable);
+        var response = employeeService.getAllEmployees(pageable).map(EmployeeResponse::from);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    /**
+     * {@code GET  /employees/:id} : get the "id" employee.
+     *
+     * @param id the id of the employee to retrieve
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the employee, or with status {@code 204 (NO_CONTENT)}
+     */
     @GetMapping("/employees/{id}")
-    @ResponseBody
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
-        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
-        if (optionalEmployee.isPresent()) {
-            return ResponseEntity.ok(optionalEmployee.get());
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<EmployeeResponse> getEmployeeById(@PathVariable Long id) {
+        LOG.info("Starting to get employee with id: {}", id);
+
+        Optional<Employee> optionalEmployee = employeeService.getEmployeeById(id);
+
+        return optionalEmployee.map(employee -> ResponseEntity.ok().body(EmployeeResponse.from(employee)))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+
     }
 
-    // update employee rest api
+    /**
+     * {@code PUT  /employees/:id} : Updates an existing employee.
+     *
+     * @param id              the id of the employee to update
+     * @param employeeDetails the employee to update
+     * @return the {@link ResponseEntity} with status {@code 202 (ACCEPTED)} and with body the updated employee,
+     */
     @PutMapping("/employees/{id}")
-    @ResponseBody
-    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
-        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
-        if (!optionalEmployee.isPresent()) {
+    public ResponseEntity<EmployeeResponse> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        LOG.info("Starting to update employee with id: {}", id);
+
+        Optional<Employee> optionalEmployee = employeeService.updateEmployee(id, employeeDetails);
+        if (optionalEmployee.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
-        Employee employee = optionalEmployee.get();
-        employee.setFirstName(employeeDetails.getFirstName());
-        employee.setLastName(employeeDetails.getLastName());
+        var response = EmployeeResponse.from(optionalEmployee.get());
+        LOG.info("Employee updated: {}", response);
 
-        Employee updatedEmployee = employeeRepository.save(employee);
-        return ResponseEntity.ok(updatedEmployee);
+        return ResponseEntity.accepted().body(response);
     }
 
-    // delete employee rest api
+    /**
+     * {@code DELETE  /employees/:id} : delete the "id" employee.
+     *
+     * @param id the id of the employee to delete
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)} or {@code 404 (NOT_FOUND)} or {@code 202 (ACCEPTED)}
+     */
     @DeleteMapping("/employees/{id}")
-    @ResponseBody
     public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
-        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
-        if (!optionalEmployee.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        Employee employee = optionalEmployee.get();
-        employeeRepository.delete(employee);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-        return ResponseEntity.ok(response);
+        LOG.info("Starting to delete employee with id: {}", id);
+        return ResponseEntity.accepted().body(employeeService.deleteEmployee(id));
     }
 }
